c3db803cb46cbb4846930712c03988e83edf57aa
Ismael-Cristino <ismcridav@alu.edu.gva.es>
2025-06-09 12:32:15 +0200

    prueba 2 rutas

--- controllers/formularioController.php
+++ controllers/formularioController.php
@@ -75,12 +75,12 @@ class formularioController
         if ($id == null) {
             $_SESSION["errores"] = $errores;
             $_SESSION["datos"] = $arrayDatos;
-            header("location:index.php?tabla=$origen&accion=ir&error=true#inicio-3");
+            header("Location: /index.php?tabla=$origen&accion=ir&enviado=true#inicio-3");
             exit();
         } else {
             unset($_SESSION["errores"]);
             unset($_SESSION["datos"]);
-            header("location:index.php?tabla=$origen&accion=ir&enviado=true#inicio-3");
+            header("Location: /index.php?tabla=$origen&accion=ir&enviado=true#inicio-3");
             exit();
         }
     }
