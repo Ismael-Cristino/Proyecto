83fe060c230a6acbb99b5b32498686d8bf44dcd4
Ismael-Cristino <ismcridav@alu.edu.gva.es>
2025-06-09 13:20:06 +0200

    cambiando titulo

--- views/layout/head.php
+++ views/layout/head.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <title>APP MVC Y PDO </title>
+    <title>MUDANZAS LOGISTICA</title>
     <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
